64ba1bf712ead529cd2053e5a0e9a3395512c174
patrieli <elias@patrikainen.fi>
2020-02-23 13:44:18 +0200

    14 and nhl

---
+++ viikko1/nhl/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,77 @@
+package ohtuesimerkki;
+
+import ohtuesimerkki.*;
+
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+ 
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void playerSearch() {
+        Player p = stats.search("Yzerman");
+        
+        assertEquals("Yzerman", p.getName());
+    }
+
+    @Test
+    public void nonPlayerSearch() {
+        Player p = stats.search("Rinne");
+
+        assertEquals(null, p);
+    }
+
+    @Test
+    public void topScorer() {
+        List<Player> l = stats.topScorers(1);
+
+        assertEquals("Gretzky", l.get(0).getName());
+    }
+
+    @Test
+    public void topScorersNull() {
+        List<Player> l = stats.topScorers(-1);
+
+        assertEquals(true, l.isEmpty());
+    }
+
+    @Test
+    public void searchByTeam() {
+        List<Player> team = stats.team("EDM");
+
+        assertEquals(3, team.size());
+    }
+    
+
+}
