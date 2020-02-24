c1f40b2c2830ae7fbeb0cb52680fa8a570c7b22b
patrieli <elias@patrikainen.fi>
2020-02-24 20:17:51 +0200

    nhl unnecessary imports removed and viikko2 tehtava 1

--- viikko1/nhl/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ viikko1/nhl/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -1,17 +1,10 @@
 package ohtuesimerkki;
 
-import ohtuesimerkki.*;
-
 import java.util.*;
-import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class StatisticsTest {
  
